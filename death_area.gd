05f70056822e186503bf04ed588ffcde78024e54
Kuaku <kuaku.email@gmail.com>
2025-04-27 13:14:44 +0200

    Added death box under map

---
+++ death_area.gd
@@ -0,0 +1,8 @@
+extends Area3D
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if body is Player:
+		(body as Player).damage(1000000000, Vector3.ZERO)
+	if body is Enemy:
+		(body as Enemy).dmg(1000000000000000)
