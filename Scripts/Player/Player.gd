054326cc3ab7226678583def7da8f8c46d4df204
Mika <mika.fynn.von.reeken@student.ibs-ol.de>
2025-05-01 23:18:33 +0200

    [REF] CE-Code-Refactor: Finished refactor of code

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -1,49 +1,63 @@
 class_name Player
 extends CharacterBody3D
 
-var speed
-const WALK_SPEED = 5.0
-const SPRINT_SPEED = 8.0
-const JUMP_VELOCITY = 4.8
-const SENSITIVITY = 0.004
-
-#bob variables
-const BOB_FREQ = 2.4
-const BOB_AMP = 0.08
-var t_bob = 0.0
-
-#fov variables
-const BASE_FOV = 90.0
-const FOV_CHANGE = 1.5
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = 9.81
-var sprayCount = 0
-
-# Bullets (from Watergun)
-var bullet = preload("res://Scenes/Player/projectile.tscn")
-var instanceItem
-@onready var gunPoint = $Head/Camera3D/InvisibleGun/InvisibleGun
-@onready var health_player = $HealthSound
-@onready var gun_player = $GundSound
-@onready var damage_player = $DamagePlayer
-@onready var ground_ray = $GroundRay
-@onready var head = $Head
-@onready var camera = $Head/Camera3D
-@onready var damage_flash_rect = $Control/CanvasLayer/DamageFlesh
-@onready var healthbar = $Control/CanvasLayer/HealthBar
+@export var walk_speed = 5.0
+@export var sprint_speed = 8.0
+@export var jump_velocity = 4.8
+@export var sensitivity = 0.004
+@export var gravity = 9.81
+@export var bob_freq = 2.4
+@export var bob_amp = 0.08
+@export var base_fov = 90.0
+@export var fov_change = 1.5
+@export var bullet_scene: PackedScene
 @export var damage_flash_time: float = 0.4
 @export var max_hp = 100
 @export var damage_indicator_scene: PackedScene
+@export var wool_scene: PackedScene
+@export var cucumber_scene: PackedScene
 
-var damage_sounds = [
+@onready var _gun_point = $Head/Camera3D/InvisibleGun/InvisibleGun
+@onready var _health_player = $HealthSound
+@onready var _gun_player = $GundSound
+@onready var _damage_player = $DamagePlayer
+@onready var _ground_ray = $GroundRay
+@onready var _head = $Head
+@onready var _camera = $Head/Camera3D
+@onready var _damage_flash_rect = $Control/CanvasLayer/DamageFlesh
+@onready var _healthbar = $Control/CanvasLayer/HealthBar
+@onready var _item_selection_overlay = $Control/CanvasLayer/PanelContainer/MarginContainer/ItemSelectionOverlay
+@onready var _wool_cooldown_timer = $woolCooldown
+@onready var _panel_cover_0 = $Control/CanvasLayer/PanelContainer/MarginContainer/ItemSelectionOverlay/Panel0/panel_cover_0
+@onready var _cucumber_cooldown_timer = $cucumberCooldown
+@onready var _panel_cover_1 = $Control/CanvasLayer/PanelContainer/MarginContainer/ItemSelectionOverlay/Panel1/panel_cover_1
+		
+var _can_plant_cucumber = true
+var _current_item = 0
+var _previous_item_node
+var _t_bob = 0.0
+var _speed
+var _sprayCount = 0
+var _instance_item
+var _step_interval = 0.3
+var _step_timer = 0.0
+var _hp = max_hp
+var _damage_flash_tween: Tween = null
+var _cooldown = 0.5 
+var _time_since_action = 0.0
+var _can_plant_wool = true
+
+const SELECTED_ITEM_BORDER = preload("res://assets/items/selected_item.tres")
+const UNSELECTED_ITEM_BORDER = preload("res://assets/items/unselected_item.tres")
+
+const DAMAGE_SOUNDS = [
 	preload("res://assets/sounds/meow1.wav"),
 	preload("res://assets/sounds/meow2.wav"),
 	preload("res://assets/sounds/meow3.wav"),
 	preload("res://assets/sounds/meow4.wav"),
 ]
 
-var footstep_sounds = {
+const FOOTSTEP_SOUNDS = {
 	"fur": [
 		preload("res://assets/sounds/Carpet_Step1.wav"),
 		preload("res://assets/sounds/Carpet_Step2.wav"),
@@ -61,63 +75,55 @@ var footstep_sounds = {
 	]
 }
 
-var step_interval = 0.3
-var step_timer = 0.0
-
-var hp = max_hp
-var damage_flash_tween: Tween = null
-var cooldown = 0.5  # Sekunden
-var time_since_action = 0.0
-
 func _ready():
 	randomize()
-	healthbar.init_health(max_hp)
-	previousItemNode = item_selection_overlay.get_node("Panel0") #start the game with the first item selected
+	_healthbar.init_health(max_hp)
+	_previous_item_node = _item_selection_overlay.get_node("Panel0") #start the game with the first item selected
 	_select_item(0)
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	var holdingItem = $Control/CanvasLayer/HoldingItem
 	holdingItem.texture = load("res://assets/items/watergun.png")
-	time_since_action = cooldown
+	_time_since_action = _cooldown
 	$woolCooldown.timeout.connect(_on_wool_cooldown_end)
 	$cucumberCooldown.timeout.connect(_on_cucumber_cooldown_end)
 	
 
 func heal(amount: float):
-	health_player.play()
-	hp += amount
-	healthbar.health = hp
+	_health_player.play()
+	_hp += amount
+	_healthbar.health = _hp
 		
 func damage(dmg: float, source_position):
-	hp -= dmg
-	healthbar.health = hp
-	camera.trigger_shake()
-	if damage_flash_tween:
-		damage_flash_tween.stop()
-		damage_flash_tween = null
+	_hp -= dmg
+	_healthbar.health = _hp
+	_camera.trigger_shake()
+	if _damage_flash_tween:
+		_damage_flash_tween.kill()
+		_damage_flash_tween = null
 	
-	if damage_player and damage_sounds.size() > 0:
-		var random_damage_sound = damage_sounds[randi() % damage_sounds.size()]
-		damage_player.stream = random_damage_sound
-		damage_player.pitch_scale = randf_range(0.95, 1.05)
-		damage_player.volume_db =  remap(min(50, dmg), 0, 50, -15, 2)
-		damage_player.play()
+	if _damage_player and DAMAGE_SOUNDS.size() > 0:
+		var random_damage_sound = DAMAGE_SOUNDS[randi() % DAMAGE_SOUNDS.size()]
+		_damage_player.stream = random_damage_sound
+		_damage_player.pitch_scale = randf_range(0.95, 1.05)
+		_damage_player.volume_db =  remap(min(50, dmg), 0, 50, -15, 2)
+		_damage_player.play()
 	
-	damage_flash_tween = create_tween()
-	damage_flash_tween.tween_property($Control/CanvasLayer/DamageFlesh, "modulate", Color(1, 1, 1, 0.6), damage_flash_time/2.0)
-	damage_flash_tween.tween_property($Control/CanvasLayer/DamageFlesh, "modulate", Color.TRANSPARENT, damage_flash_time/2.0)
+	_damage_flash_tween = create_tween()
+	_damage_flash_tween.tween_property($Control/CanvasLayer/DamageFlesh, "modulate", Color(1, 1, 1, 0.6), damage_flash_time/2.0)
+	_damage_flash_tween.tween_property($Control/CanvasLayer/DamageFlesh, "modulate", Color.TRANSPARENT, damage_flash_time/2.0)
 	
 	var indicator = damage_indicator_scene.instantiate()
 	indicator.damage_source_position = source_position
 	indicator.player = self
-	indicator.camera = camera
+	indicator.camera = _camera
 	$Control/CanvasLayer.add_child(indicator)
-	if hp <= 0:
+	if _hp <= 0:
 		$Control/CanvasLayer/death_menu.failLevel()
 		$Control/CanvasLayer.remove_child(indicator)
 
 func get_ground_type() -> String:
-	if ground_ray.is_colliding():
-		var collider = ground_ray.get_collider()
+	if _ground_ray.is_colliding():
+		var collider = _ground_ray.get_collider()
 		if collider.has_meta("ground_type"):
 			return collider.get_meta("ground_type")
 	return "default"
@@ -128,9 +134,9 @@ func _unhandled_input(event):
 		$pause_menu.pause()
 	
 	if event is InputEventMouseMotion:
-		head.rotate_y(-event.relative.x * SENSITIVITY)
-		camera.rotate_x(-event.relative.y * SENSITIVITY)
-		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-60), deg_to_rad(60))
+		_head.rotate_y(-event.relative.x * sensitivity)
+		_camera.rotate_x(-event.relative.y * sensitivity)
+		_camera.rotation.x = clamp(_camera.rotation.x, deg_to_rad(-60), deg_to_rad(60))
 
 # UI item selection
 	if event is InputEventKey:
@@ -147,11 +153,11 @@ func _unhandled_input(event):
 	if event is InputEventMouseButton:
 		if event.pressed:
 			if event.button_index == MOUSE_BUTTON_WHEEL_UP:
-				_select_item((currentItem + 1) % 4)
+				_select_item((_current_item + 1) % 4)
 			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
-				if currentItem == 0:
+				if _current_item == 0:
 					_select_item(3)
-				else: _select_item((currentItem - 1) % 3)
+				else: _select_item((_current_item - 1) % 3)
 	
 func _process(delta):
 	if global_transform.origin.y < -30:
@@ -159,7 +165,7 @@ func _process(delta):
 
 func play_footstep_sound():
 	var ground_type = get_ground_type()
-	var sounds = footstep_sounds.get(ground_type, null)
+	var sounds = FOOTSTEP_SOUNDS.get(ground_type, null)
 	if sounds == null:
 		return
 	if sounds.size() > 0:
@@ -167,7 +173,7 @@ func play_footstep_sound():
 		$StepPlayer.stream = random_sound
 		$StepPlayer.pitch_scale = randf_range(0.95, 1.05)
 		$StepPlayer.play()
-		if speed == WALK_SPEED:
+		if _speed == walk_speed:
 			$StepPlayer.volume_db = 6
 		else:
 			$StepPlayer.volume_db = 10
@@ -175,12 +181,12 @@ func play_footstep_sound():
 		
 func _physics_process(delta):
 	if is_on_floor() and velocity.length() > 1.0:
-		step_timer += delta * (velocity.length() / speed * 0.9)
-		if step_timer >= step_interval:
-			step_timer = 0
+		_step_timer += delta * (velocity.length() / _speed * 0.9)
+		if _step_timer >= _step_interval:
+			_step_timer = 0
 			play_footstep_sound()
 	else:
-		step_timer = step_interval
+		_step_timer = _step_interval
 	
 	# Add the gravity.
 	if not is_on_floor():
@@ -188,103 +194,81 @@ func _physics_process(delta):
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 	
 	# Handle Sprint.
 	if Input.is_action_pressed("sprint"):
-		speed = SPRINT_SPEED
+		_speed = sprint_speed
 	else:
-		speed = WALK_SPEED
+		_speed = walk_speed
 	
 	if Input.is_action_pressed("action"):
-		if sprayCount <= 2:
-			gun_player.play()
-		if sprayCount < 15:
-			instanceItem = bullet.instantiate()
-			(instanceItem as Projectile).damage = 1
-			instanceItem.position = gunPoint.global_position
-			instanceItem.transform.basis = gunPoint.global_transform.basis
-			get_parent().add_child(instanceItem)
-			sprayCount += 1
-			# PUT ANIMATION HERE
-		# NOT HERE
-	if time_since_action > 0.5:
-		time_since_action = 0
-		sprayCount = 0
-	time_since_action += delta
+		if _sprayCount <= 2:
+			_gun_player.play()
+		if _sprayCount < 15:
+			_instance_item = bullet_scene.instantiate()
+			(_instance_item as Projectile).shoot(1, _gun_point.global_position, _gun_point.global_transform.basis)
+			get_parent().add_child(_instance_item)
+			_sprayCount += 1
+	if _time_since_action > 0.5:
+		_time_since_action = 0
+		_sprayCount = 0
+	_time_since_action += delta
 	
-	
-	# Get the input direction and handle the movement/deceleration.
 	var input_dir = Input.get_vector("left", "right", "up", "down")
-	var direction = (head.transform.basis * transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (_head.transform.basis * transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if is_on_floor():
 		if direction:
-			velocity.x = direction.x * speed
-			velocity.z = direction.z * speed
+			velocity.x = direction.x * _speed
+			velocity.z = direction.z * _speed
 		else:
-			velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
-			velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
+			velocity.x = lerp(velocity.x, direction.x * _speed, delta * 7.0)
+			velocity.z = lerp(velocity.z, direction.z * _speed, delta * 7.0)
 	else:
-		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
-		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
+		velocity.x = lerp(velocity.x, direction.x * _speed, delta * 3.0)
+		velocity.z = lerp(velocity.z, direction.z * _speed, delta * 3.0)
 	
 	# Head bob
-	t_bob += delta * velocity.length() * float(is_on_floor())
-	camera.transform.origin = _headbob(t_bob)
+	_t_bob += delta * velocity.length() * float(is_on_floor())
+	_camera.transform.origin = _headbob(_t_bob)
 	
 	# FOV
-	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
-	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
-	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
+	var velocity_clamped = clamp(velocity.length(), 0.5, sprint_speed * 2)
+	var target_fov = base_fov + fov_change * velocity_clamped
+	_camera.fov = lerp(_camera.fov, target_fov, delta * 8.0)
 	
 	move_and_slide()
 
 
 func _headbob(time) -> Vector3:
 	var pos = Vector3.ZERO
-	pos.y = sin(time * BOB_FREQ) * BOB_AMP
-	pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
+	pos.y = sin(time * bob_freq) * bob_amp
+	pos.x = cos(time * bob_freq / 2) * bob_amp
 	return pos
-
-
-@onready var item_selection_overlay = $Control/CanvasLayer/PanelContainer/MarginContainer/ItemSelectionOverlay
-
-const SELECTED_ITEM_BORDER = preload("res://assets/items/selected_item.tres")
-const UNSELECTED_ITEM_BORDER = preload("res://assets/items/unselected_item.tres")
-#var itemInventory = [wollbombe, wollbombe, wollbombe, wollbombe]
-var currentItem = 0
-var previousItemNode
 
 # Displays the selected item in the UI
 func _select_item(selectedItemSlot):
-	previousItemNode.add_theme_stylebox_override("panel", UNSELECTED_ITEM_BORDER)
-	currentItem = selectedItemSlot
+	_previous_item_node.add_theme_stylebox_override("panel", UNSELECTED_ITEM_BORDER)
+	_current_item = selectedItemSlot
 	var item_node_name = "Panel" + str(selectedItemSlot)
-	var item_node = item_selection_overlay.get_node(item_node_name)
+	var item_node = _item_selection_overlay.get_node(item_node_name)
 	item_node.add_theme_stylebox_override("panel", SELECTED_ITEM_BORDER)
-	previousItemNode = item_node
+	_previous_item_node = item_node
 
 func _input(event):
 	if event.is_action_pressed("useItem"):
-		match currentItem:
+		match _current_item:
 			#wool
 			0:
 				drop_wool()
 			1:
 				drop_cucumber()
 
-@export var wool_scene: PackedScene
-var can_plant_wool = true
-@onready var wool_cooldown_timer = $woolCooldown
-@onready var panel_cover_0 = $Control/CanvasLayer/PanelContainer/MarginContainer/ItemSelectionOverlay/Panel0/panel_cover_0
-
-
-
 func drop_wool():
 	if not wool_scene:
 		print("⚠️ No bomb scene assigned!")
 		return
-	if can_plant_wool:
+	if _can_plant_wool:
 		var wool = wool_scene.instantiate()
 		wool.name = "wool"  # Optional: makes it easier to find via code
 
@@ -292,25 +276,20 @@ func drop_wool():
 		wool.global_transform.origin = global_transform.origin + Vector3(0, 0, -1)
 		get_tree().current_scene.add_child(wool)
 		
-		can_plant_wool= false
-		wool_cooldown_timer.start()
-		panel_cover_0.visible = true
+		_can_plant_wool= false
+		_wool_cooldown_timer.start()
+		_panel_cover_0.visible = true
 		
 		
 func _on_wool_cooldown_end():
-	can_plant_wool = true
-	panel_cover_0.visible = false
-		
-@export var cucumber_scene: PackedScene
-var can_plant_cucumber = true
-@onready var cucumber_cooldown_timer = $cucumberCooldown
-@onready var panel_cover_1 = $Control/CanvasLayer/PanelContainer/MarginContainer/ItemSelectionOverlay/Panel1/panel_cover_1
+	_can_plant_wool = true
+	_panel_cover_0.visible = false
 
 func drop_cucumber():
 	if not cucumber_scene:
 		print("⚠️ No bomb scene assigned!")
 		return
-	if can_plant_cucumber:
+	if _can_plant_cucumber:
 		var cucumber = cucumber_scene.instantiate()
 		cucumber.name = "cucumber"  # Optional: makes it easier to find via code
 
@@ -318,11 +297,11 @@ func drop_cucumber():
 		cucumber.global_transform.origin = global_transform.origin + Vector3(0, 0, -1)
 		get_tree().current_scene.add_child(cucumber)
 		
-		can_plant_cucumber= false
-		cucumber_cooldown_timer.start()
-		panel_cover_1.visible = true
+		_can_plant_cucumber= false
+		_cucumber_cooldown_timer.start()
+		_panel_cover_1.visible = true
 	
 func _on_cucumber_cooldown_end():
-	can_plant_cucumber = true
-	panel_cover_1.visible = false
+	_can_plant_cucumber = true
+	_panel_cover_1.visible = false
 	
--- Scripts/Player/Projectiles.gd
+++ Scripts/Player/Projectiles.gd
@@ -1,41 +1,48 @@
 class_name Projectile
 extends Node3D
+
+@export var speed = 15.0
+@export var gravity = 9.81
+
+@onready var _sprite = $Sprite3D
+@onready var _ray = $RayCast3D
+@onready var _animation = $AnimatedSprite3D
+
 var _camera: Camera3D
 var _player: Node3D
-const SPEED = 15.0
-const GRAVITY = 9.81
-var gravitation = 1
-var bodyEntered = false
-var velocity
-@onready var sprite = $Sprite3D
-@onready var ray = $RayCast3D
-@onready var animation = $AnimatedSprite3D
-var damage = 0
+var _damage = 0
+var _gravitation = 1
+var _bodyEntered = false
+var _velocity
 
 func _ready():
 	_camera = get_viewport().get_camera_3d()
-	 
+
+func shoot(dmg: float, postion: Vector3, base: Basis):
+	_damage = dmg
+	position = postion
+	transform.basis = base
 
 func _rotate_to_player():
 	var to_camera = _camera.global_transform.origin - global_transform.origin
 	to_camera.y = 0
 	
 	var rot_y = atan2(to_camera.x, to_camera.z)
-	sprite.rotation.y = rot_y
+	_sprite.rotation.y = rot_y
 		
 func _process(delta: float) -> void:
-	if (bodyEntered):
-		velocity = Vector3(0, 0, 0)
+	if (_bodyEntered):
+		_velocity = Vector3(0, 0, 0)
 	else:
-		velocity = transform.basis * Vector3(0, gravitation, -SPEED)	
-	position +=  velocity * delta
-	gravitation -= GRAVITY * delta 
-	if velocity.length() > 0.1:
-		ray.target_position = velocity.normalized() * 1.0
+		_velocity = transform.basis * Vector3(0, _gravitation, -speed)	
+	position +=  _velocity * delta
+	_gravitation -= gravity * delta 
+	if _velocity.length() > 0.1:
+		_ray.target_position = _velocity.normalized() * 1.0
 		
-	ray.force_raycast_update()
-	if ray.is_colliding():
-		var collider = ray.get_collider()
+	_ray.force_raycast_update()
+	if _ray.is_colliding():
+		var collider = _ray.get_collider()
 		hit_body(collider)
 
 func _physics_process(delta: float):
@@ -46,10 +53,10 @@ func _physics_process(delta: float):
 func hit_body(body: Node3D):
 	if body is Player:
 		return
-	if body is Enemy && !bodyEntered:
-		(body as Enemy).dmg(damage)
-	bodyEntered = true
-	animation.play("default")
+	if body is Enemy && !_bodyEntered:
+		(body as Enemy).dmg(_damage)
+	_bodyEntered = true
+	_animation.play("default")
 	
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
